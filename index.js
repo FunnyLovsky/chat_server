d286d6862ca62d324abbb20b84b936a6072bc5eb
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-11-24 16:01:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+let messages= [];
+let clients = [];
+
+app.post('/message', (req, res) => {
+  const  message = req.body;
+  messages.push(message);
+
+  clients.forEach(client => client.res.json( message ));
+  clients = [];
+
+  res.status(200).send();
+});
+
+app.get('/messages', (req, res) => {
+    const client = { res };
+    clients.push(client);
+
+  const timeoutId = setTimeout(() => {
+    const index = clients.indexOf(client);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+    if (!res.headersSent) { 
+      res.json({  });
+    }
+  }, 30000);
+
+
+  req.on('close', () => {
+    clearTimeout(timeoutId);
+    const index = clients.indexOf(client);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+  });
+});
+
+
+const start = () => {
+    try {
+        app.listen(PORT, () => console.log('Server start in port: ' + PORT))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+start()
